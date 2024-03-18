9e6b8dd9b1e6891dda60b0b78c8ee5c8efebd1d5
Andeptrai <quocanphan123@gmail.com>
2024-03-18 13:25:41 +0700

    update

--- userDisplay/assets/js/checkout.js
+++
@@ -1,9 +0,0 @@
-document.querySelector('.buttonCheckout').addEventListener('click', function () {
-    performCheckout();
-
-    alert('Checkout successful! Thank you for your purchase.');
-
-    window.location.href = 'index.html';
-});
-
-
