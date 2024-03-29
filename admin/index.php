8d24e656e6758f174a33f5f0cbd33a3d0dca7a73
Tiendat16 <134023823+tiendat1631@users.noreply.github.com>
2024-03-29 17:08:42 +0700

    Update index.php

--- admin/index.php
+++ admin/index.php
@@ -57,7 +57,7 @@
     }
     ?>
 
-
+        <!-- tên nav reponsive màu nav  -->
         <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
             <!-- Navbar Brand-->
             <a class="navbar-brand ps-3" href="index.html">Start Bootstrap</a>
