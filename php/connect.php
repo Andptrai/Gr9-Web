b3bbd0cb8240a1d3c4746cea3dc74651717f3afe
Andeptrai <quocanphan123@gmail.com>
2024-04-29 20:43:01 +0700

    a

--- php/connect.php
+++ php/connect.php
@@ -1,10 +1,10 @@
 <?php
- $servername = "localhost";
- $username = "root";
- $password = "";
- $dbname = "web2";
- //$port = '3307';
- $conn = new mysqli($servername, $username, $password, $dbname);
+  $servername = "localhost";
+  $username = "root";
+  $password = "";
+  $dbname = "web2";
+  $port = '3307';
+  $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
  if ($conn->connect_error){
      die("Connection failed: " . $conn->connect_error);
