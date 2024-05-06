3d428b3cd6c6a9b3a1bbf978cafec2dd469834df
Andeptrai <quocanphan123@gmail.com>
2024-05-06 20:13:15 +0700

    Update index.php

--- interface/index.php
+++ interface/index.php
@@ -3,6 +3,7 @@ include '../php/header.php';
 ?>
 
 
+
 	<!-- Slider -->
 	<section class="section-slide">
 		<div class="wrap-slick1">
@@ -23,11 +24,9 @@ include '../php/header.php';
 							</div>
 
 							<div class="layer-slick1 animated visible-false" data-appear="zoomIn" data-delay="1600">
-								<button  data-filter=".women">
-									<a href="product.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-										Shop Now
-									</a>
-								</button>
+								<a href="product.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
+									Shop Now
+								</a>
 							</div>
 						</div>
 					</div>
@@ -49,11 +48,9 @@ include '../php/header.php';
 							</div>
 
 							<div class="layer-slick1 animated visible-false" data-appear="slideInUp" data-delay="1600">
-								<button  data-filter=".men">
-									<a href="product.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-										Shop Now
-									</a>
-								</button>
+								<a href="product.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
+									Shop Now
+								</a>
 							</div>
 						</div>
 					</div>
@@ -75,11 +72,9 @@ include '../php/header.php';
 							</div>
 
 							<div class="layer-slick1 animated visible-false" data-appear="rotateIn" data-delay="1600">
-								<button  data-filter=".men">
-									<a href="product.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
-										Shop Now
-									</a>
-								</button>
+								<a href="product.php" class="flex-c-m stext-101 cl0 size-101 bg1 bor1 hov-btn1 p-lr-15 trans-04">
+									Shop Now
+								</a>
 							</div>
 						</div>
 					</div>
@@ -98,12 +93,12 @@ include '../php/header.php';
 					<div class="block1 wrap-pic-w " > 
 						<img src="images/banner-01.jpg" alt="IMG-BANNER">
 
-						<a href="#" class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3" data-filter=".women">
+						<div  class="block1-txt ab-t-l s-full flex-col-l-sb p-lr-38 p-tb-34 trans-03 respon3" >
 							<div class="block1-txt-child1 flex-col-l">
 								<span class="block1-name ltext-102 trans-04 p-b-8">
 									Women
 								</span>
-						
+
 								<span class="block1-info stext-102 trans-04">
 									Spring 2018
 								</span>
@@ -429,37 +424,37 @@ include '../php/header.php';
 				<div class="row">
 
 
-				<?php foreach ($products as $product): ?>
-			<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item <?php echo $product['category']; ?>">
-				<div class="block2">
-					<div class="block2-pic hov-img0">
-						<img src="<?php echo $product['image']; ?>" alt="IMG-PRODUCT">
-						<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1" data-product-id="<?php echo $product['idProduct']; ?>">
-							Quick View
-						</a>
+					<?php foreach ($products as $product): ?>
+					<div class="col-sm-6 col-md-4 col-lg-3 p-b-35 isotope-item <?php echo $product['category']; ?>">
+						<div class="block2">
+							<div class="block2-pic hov-img0">
+								<img src="<?php echo $product['image']; ?>" alt="IMG-PRODUCT">
+								<a href="#" class="block2-btn flex-c-m stext-103 cl2 size-102 bg0 bor2 hov-btn1 p-lr-15 trans-04 js-show-modal1" data-product-id="<?php echo $product['idProduct']; ?>">
+									Quick View
+								</a>
 
-					</div>
-					<div class="block2-txt flex-w flex-t p-t-14">
-						<div class="block2-txt-child1 flex-col-l ">
-							<a href="product-detail.php?product_id=<?php echo $product['idProduct']; ?>"  class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
-								<?php echo $product['name']; ?>
-							</a>
+							</div>
+							<div class="block2-txt flex-w flex-t p-t-14">
+								<div class="block2-txt-child1 flex-col-l ">
+									<a href="product-detail.php?product_id=<?php echo $product['idProduct']; ?>"  class="stext-104 cl4 hov-cl1 trans-04 js-name-b2 p-b-6">
+										<?php echo $product['name']; ?>
+									</a>
 
-							<span class="stext-105 cl3">
-								<?php echo $product['price']; ?>
-							</span>
-						</div>
-						<div class="block2-txt-child2 flex-r p-t-3">
-							<a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
-								<img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
-								<img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
-							</a>
+									<span class="stext-105 cl3">
+										<?php echo $product['price']; ?>
+									</span>
+								</div>
+								<div class="block2-txt-child2 flex-r p-t-3">
+									<a href="#" class="btn-addwish-b2 dis-block pos-relative js-addwish-b2">
+										<img class="icon-heart1 dis-block trans-04" src="images/icons/icon-heart-01.png" alt="ICON">
+										<img class="icon-heart2 dis-block trans-04 ab-t-l" src="images/icons/icon-heart-02.png" alt="ICON">
+									</a>
+								</div>
+							</div>
 						</div>
 					</div>
+					<?php endforeach; ?>
 				</div>
-			</div>
-			<?php endforeach; ?>
-			</div>
 			</div>
 
 			<!-- Load more -->
