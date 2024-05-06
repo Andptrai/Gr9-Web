7c2cfe6e55afa9a5cb69fe194767101d5cf3683b
TNTDanh <thainguyenthanhdanhmh@gmail.com>
2024-05-07 02:27:15 +0700

    225

--- php/header.php
+++ php/header.php
@@ -118,23 +118,13 @@ if (isset($_GET['product_id'])) {
 		 <!-- Header desktop  -->
 		<div class="container-menu-desktop">
 			 <!-- Topbar  -->
-			<div class="top-bar">
-				<div class="content-topbar flex-sb-m h-full container">
-					<div class="right-top-bar flex-w h-full">
-
-						<a href="#" class="flex-c-m trans-04 p-lr-25">
-							My Account
-						</a>
-   
-					</div>
-				</div>
-			</div>
+			
 
 			<div class="wrap-menu-desktop how-shadow1">
 				<nav class="limiter-menu-desktop container">
 					
 					<!-- Logo desktop -->		
-					<a href="#" class="logo">
+					<a href="" class="logo">
 						<img src="images/icons/logo-01.png" alt="IMG-LOGO">
 					</a>
 
@@ -178,10 +168,10 @@ if (isset($_GET['product_id'])) {
 							<i class="zmdi zmdi-shopping-cart"></i>
 						</div>
 
-
 						<a href="#" class="icon-header-item cl2 hov-cl1 trans-04 p-l-22 p-r-11 icon-header-noti" data-notify="0">
 							<i class="zmdi zmdi-favorite-outline"></i>
 						</a>
+						
 					</div>
 				</nav>
 			</div>	
